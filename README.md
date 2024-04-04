08fb8c276c08d94d3be21904c9eca7d999968959
ChanningTong <Channing_TongCN@outlook.com>
2024-04-04 22:33:46 +0800

    init flutter

---
+++ README.md
@@ -0,0 +1,7 @@
+# intellij_tourism_designer
+
+CUG萌新esriGIS开发组玩具.
+
+## Kanban
+- Mobile UI build
+- Web UI build
---
+++ android/app/src/main/kotlin/com/example/intellij_tourism_designer/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.intellij_tourism_designer
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
