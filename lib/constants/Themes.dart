8aa42e4548c75a44a716eafa38a3ceb16bcf0ade
MinWenaaa <163091721+MinWenaaa@users.noreply.github.com>
2024-04-19 17:50:52 +0800

    [feat] Add color scheme

---
+++ lib/constants/Themes.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+
+class AppColors1 {
+  AppColors1._();
+
+  static const Color primaryColor = Color(0xFFFFD700); // 主色调
+  static const Color secondaryColor = Color(0xffb0b0b0); // 辅助色
+  static const Color accentColor = Color(0xff00008b); // 强调色
+  static const Color backgroundColor = Color(0xFFF5F5F5); // 背景色
+  static const Color textColor = Color(0xff4f4f4f); // 文本颜色
+}
